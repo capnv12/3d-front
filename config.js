ba6c26dcd8a1e00dbd708a4cd53b2a8203f9a684
capn12 <capn12@gmail.com>
2020-02-20 23:39:18 +0200

    corrected

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
-import getConfig from 'next/config'
-const { publicRuntimeConfig } = getConfig()
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
 
-export const API = publicRuntimeConfig.PRODUCTION ? 'http://138.68.110.69/api' : 'http://localhost:8000/api'
-export const APP_NAME = publicRuntimeConfig.APP_NAME
-export const DOMAIN = publicRuntimeConfig.PRODUCTION ? publicRuntimeConfig.DOMAIN_DEVELOPMENT : publicRuntimeConfig.DOMAIN_PRODUCTION
+export const API = publicRuntimeConfig.PRODUCTION
+	? 'http://46.101.208.104/api'
+	: 'http://localhost:8000/api';
+export const APP_NAME = publicRuntimeConfig.APP_NAME;
+export const DOMAIN = publicRuntimeConfig.PRODUCTION
+	? publicRuntimeConfig.DOMAIN_DEVELOPMENT
+	: publicRuntimeConfig.DOMAIN_PRODUCTION;
 // export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME
 // export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
